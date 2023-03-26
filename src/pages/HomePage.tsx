e9cb251aaf422e6c61a38101c5af7ca609708a46
dotrieuco1986 <donguyendy@gmail.com>
2023-03-26 14:58:20 +0700

    Root Commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import MovieList from '../movies/MovieList';
 
 const HomePage = () => {
+
+  const [videoType, setVideoType] = useState('now_playing');
   return (
     <Fragment>
       <section className='movies-layout page-container pb-10'>
-        <h2 className='capitalize text-white mb-10 text-3xl font-bold'>
-          Now Playing
-        </h2>
-        <MovieList></MovieList>
-      </section>
-      <section className='movies-layout page-container pb-10'>
-        <h2 className='capitalize text-white mb-10 text-3xl font-bold'>
-          Top Rated
-        </h2>
-        <MovieList type='top_rated'></MovieList>
+        <div className='flex flex-direction-column'>
+          <h2 className='capitalize text-white mb-10 text-3xl font-bold' onClick={() => setVideoType('now_playing')}>
+            Now Playing
+          </h2>
+          <h2 className='capitalize text-white mb-10 text-3xl font-bold ml-10' onClick={() => setVideoType('top_rated')}>
+            Top Rated
+          </h2>
+        </div>
+        <MovieList type={videoType}></MovieList>
       </section>
       <section className='movies-layout page-container pb-10'>
         <h2 className='capitalize text-white mb-10 text-3xl font-bold'>
